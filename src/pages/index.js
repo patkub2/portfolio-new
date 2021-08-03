65cc964dcf34fd742e6f2eb70a4acd39115b3d0a
patkub2 <patrykkubala1999@gmail.com>
2021-08-03 16:59:36 +0200

    added pictures

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,26 +4,15 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-
+import im1 from "../images/1.png"
+import im2 from "../images/2.png"
+import { ParallaxBanner } from "react-scroll-parallax"
+import { ParallaxProvider } from "react-scroll-parallax"
 const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
+  <ParallaxProvider>
+    <img src={im1} />
+    <img src={im2} />
+  </ParallaxProvider>
 )
 
 export default IndexPage
