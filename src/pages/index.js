332db7f51127657906c770dc3ab95d67b7d9fc39
patkub2 <patrykkubala1999@gmail.com>
2021-08-15 23:51:46 +0200

    changed div grid to bg

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,120 +11,104 @@ import im6 from "../images/6.png"
 import im7 from "../images/7.png"
 import im8 from "../images/8.png"
 import imbg from "../images/bg.png"
+import impla from "../images/bgpla.png"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
 
 gsap.registerPlugin(ScrollTrigger)
 
 const Container = styled.div`
-  /*border: 1px solid red; /* BORDER TEST*/
+  border: 1px solid red; /* BORDER TEST*/
   padding: 0;
   margin: 0;
+  height: 100vh;
+
   //background-color: #130d0a;
   font-family: "Playfair Display", serif;
   color: #fff;
-  img {
-    position: absolute;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  .im1 {
+    z-index: 1;
+    background-image: url(${im1});
   }
+  .im2 {
+    z-index: 2;
+    background-image: url(${im2});
+  }
+  .im3 {
+    z-index: 3;
+    background-image: url(${im3});
+  }
+  .im4 {
+    z-index: 4;
+    background-image: url(${im4});
+  }
+  .im5 {
+    z-index: 5;
+    background-image: url(${im5});
+  }
+  .im6 {
+    z-index: 6;
+    background-image: url(${im6});
+  }
+  .im7 {
+    z-index: 7;
+    background-image: url(${im7});
+  }
+  .im8 {
+    z-index: 8;
+    background-image: url(${im8});
+  }
+  .impla {
+    z-index: 9;
+    background-image: url(${impla});
+  }
+  .imbgcolor {
+    z-index: 0;
+    background: linear-gradient(
+      180deg,
+      rgba(165, 212, 234, 1) 52%,
+      rgba(2, 13, 23, 1) 53%
+    );
+  }
+  .imbg {
+    z-index: 1;
+    background-image: url(${imbg});
+  }
+`
+const Box = styled.div`
+  position: absolute;
+  border: 1px solid red; /* BORDER TEST*/
+  padding: 0;
+  margin: 0;
+  width: 50vw;
+  height: 100vh;
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
 `
 const Space = styled.div`
   min-height: 2000px;
 `
 function IndexPage() {
-  const wrapper = useRef(null)
-
-  useEffect(() => {
-    gsap.to(".im1", {
-      yPercent: -15,
-      ease: "none",
-      scrollTrigger: {
-        trigger: ".im1",
-        // start: "top bottom", // the default values
-        // end: "bottom top",
-        scrub: true,
-      },
-    })
-    gsap.to(".im2", {
-      yPercent: -5,
-      ease: "none",
-      scrollTrigger: {
-        trigger: ".im2",
-        // start: "top bottom", // the default values
-        // end: "bottom top",
-        scrub: true,
-      },
-    })
-    gsap.to(".im3", {
-      yPercent: 0,
-      ease: "none",
-      scrollTrigger: {
-        trigger: ".im3",
-        // start: "top bottom", // the default values
-        // end: "bottom top",
-        scrub: true,
-      },
-    })
-    gsap.to(".im4", {
-      yPercent: 5,
-      ease: "none",
-      scrollTrigger: {
-        trigger: ".im4",
-        // start: "top bottom", // the default values
-        // end: "bottom top",
-        scrub: true,
-      },
-    })
-    gsap.to(".im5", {
-      yPercent: 10,
-      ease: "none",
-      scrollTrigger: {
-        trigger: ".im5",
-        // start: "top bottom", // the default values
-        // end: "bottom top",
-        scrub: true,
-      },
-    })
-    gsap.to(".im6", {
-      yPercent: 15,
-      ease: "none",
-      scrollTrigger: {
-        trigger: ".im6",
-        // start: "top bottom", // the default values
-        // end: "bottom top",
-        scrub: true,
-      },
-    })
-    gsap.to(".im7", {
-      yPercent: 15,
-      ease: "none",
-      scrollTrigger: {
-        trigger: ".im7",
-        // start: "top bottom", // the default values
-        // end: "bottom top",
-        scrub: true,
-      },
-    })
-  }, [])
   return (
     <>
-      <Container ref={wrapper} className="imw">
-        <img src={imbg} className="imbg" />
-
-        <img src={im1} className="im1" />
-
-        <img src={im2} className="im2" />
-
-        <img src={im3} className="im3" />
-
-        <img src={im4} className="im4" />
-
-        <img src={im5} className="im5" />
-
-        <img src={im6} className="im6" />
-
-        <img src={im7} className="im7" />
-
-        <img src={im8} className="im2" />
+      <Container className="imw">
+        <Box></Box>
+        <Box className="impla" />
+        <Box className="imbg" />
+        <Box className="im8" />
+        <Box className="im7" />
+        <Box className="im6" />
+        <Box className="im5" />
+        <Box className="im4" />
+        <Box className="im3" />
+        <Box className="im2" />
+        <Box className="im1" />
+        <Box className="imbgcolor" />
       </Container>
+
       <Space />
     </>
   )
