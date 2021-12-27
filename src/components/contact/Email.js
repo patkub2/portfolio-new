6b21f94c5af8f553d71051247fe2174c86b8ae33
patkub2 <patrykkubala1999@gmail.com>
2021-12-27 17:00:42 +0100

    added notigications to form and error catching

--- src/components/contact/Email.js
+++ src/components/contact/Email.js
@@ -5,6 +5,9 @@ import { colors, media, typography } from "../../utils"
 import Button from "./Button"
 import "./index.css"
 import ReCAPTCHA from "react-google-recaptcha"
+import { ToastContainer, toast } from "react-toastify"
+
+import "react-toastify/dist/ReactToastify.css"
 
 const Input = styled.input`
   width: 100%;
@@ -39,48 +42,88 @@ export default function Email() {
   const [submitted, setSubmitted] = useState(false)
   const sendEmail = e => {
     e.preventDefault()
-
-    emailjs
-      .sendForm(
-        "default_service",
-        "template_7kuen38",
-        form.current,
-        process.env.GATSBY_EMAILJS
-      )
-      .then(
-        result => {
-          console.log(result.text)
-        },
-        error => {
-          console.log(error.text)
-        }
-      )
+    if (submitted) {
+      emailjs
+        .sendForm(
+          "default_service",
+          "template_7kuen38",
+          form.current,
+          process.env.GATSBY_EMAILJS
+        )
+        .then(
+          result => {
+            console.log(result.text)
+            toast.success("email sent successfully", {
+              position: "bottom-right",
+              autoClose: 3000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "colored",
+            })
+            document.getElementById("form").reset()
+          },
+          error => {
+            console.log(error.text)
+            toast.error("oh no, something went wrong", {
+              position: "bottom-right",
+              autoClose: 3000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "colored",
+            })
+          }
+        )
+    } else {
+      toast.warning("fill in the captcha", {
+        position: "bottom-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      })
+    }
   }
-
+  const notify = () => console.log(submitted)
   return (
-    <Form ref={form} onSubmit={sendEmail}>
-      <Label>Name</Label>
-      <Input type="text" name="user_name" required />
-      <Label>Email</Label>
-      <Input type="email" name="user_email" required />
-      <Label>Message</Label>
-      <Message name="message" name="user_message" required />
-      <ReCAPTCHA
-        sitekey={process.env.GATSBY_CAPTCHA}
-        onChange={() => setSubmitted(true)}
+    <>
+      <Form id="form" ref={form} onSubmit={sendEmail}>
+        <Label>Name</Label>
+        <Input type="text" name="user_name" required />
+        <Label>Email</Label>
+        <Input type="email" name="user_email" required />
+        <Label>Message</Label>
+        <Message name="message" name="user_message" required />
+        <ReCAPTCHA
+          sitekey={process.env.GATSBY_CAPTCHA}
+          onChange={() => setSubmitted(!submitted)}
+        />
+        <input
+          type="submit"
+          value="Send"
+          className="uiui-button uiui-button--glow example-1"
+        />
+      </Form>
+      <button onClick={notify}>Notify !</button>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
       />
-      <input
-        type="submit"
-        value="Send"
-        className="uiui-button uiui-button--glow example-1"
-      />
-      <button
-        onClick={() => {
-          console.log(submitted)
-        }}
-      >
-        asd
-      </button>
-    </Form>
+    </>
   )
 }
